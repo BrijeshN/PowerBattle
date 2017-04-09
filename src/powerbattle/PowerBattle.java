37dfa15025bd8de31604c8ef44c427fea37d4abe
BrijeshN <nayakb1993@gmail.com>
2017-04-09 18:37:29 -0400

    Created GitHub repository.

--- src/powerbattle/PowerBattle.java
+++ src/powerbattle/PowerBattle.java
@@ -7,6 +7,7 @@ public class PowerBattle {
         
         // Diplays window on the screen with the follwing title, width and height
         // Create Game object and store it object called game
+        System.out.println("Created By: Brijesh Nayak and Da Lin");
         Game game = new Game("Power Battle", 1000, 685);
         
         // Call start mathod of the Game, which will call other methods
